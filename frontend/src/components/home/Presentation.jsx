c52d6d756fae7198a5b812d9276fdaaf0623ac9f
hhertout <hugues.hertout@gmail.com>
2023-02-17 18:47:02 +0100

    init tailwind config

--- frontend/src/components/home/Presentation.jsx
+++ frontend/src/components/home/Presentation.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Presentation() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1 className="text-3xl">Hello World</h1>
     </div>
   )
 }
