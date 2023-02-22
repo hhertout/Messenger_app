0f2fe1b41e22b4e99d089b93c0c597c6e38a7e0f
hhertout <hugues.hertout@gmail.com>
2023-02-22 14:23:09 +0100

    fix style

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import LoginForm from '../components/organisms/form/LoginForm'
-import { login } from '../api/Auth'
+import React from "react"
+import LoginForm from "../components/organisms/form/LoginForm"
+import { login } from "../api/Auth"
 
 export default function Login() {
   return (
     <div>
+      <div className="container-sm">
+        <h2>Loggin</h2>
         <LoginForm login={login} />
+      </div>
     </div>
   )
 }
