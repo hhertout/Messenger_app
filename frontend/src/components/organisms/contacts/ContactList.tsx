b2052fe0d8387710ca4e7d86f4b8330f594839f4
hhertout <hugues.hertout@gmail.com>
2023-02-26 18:33:54 +0100

    fix

--- frontend/src/components/organisms/contacts/ContactList.tsx
+++ frontend/src/components/organisms/contacts/ContactList.tsx
@@ -6,16 +6,16 @@ import { UserContext } from "../../../contexts/UserContext"
 
 export default function ContactList() {
   const { contacts, contactNb } = useContext(UserContext)
+  console.log(contacts)
 
   return (
     <div className="contact-wrapper">
       <div>Contact number : {contactNb}</div>
-      { contacts.length > 0 ? "No contact found... "
-        : contacts.map(contact => {
+      { contacts.length >= 0 ? contacts.map(contact => {
             return (
                 <ContactCards key={uuid()} id={contact.ID} firstname={contact.Firstname} lastname={contact.Lastname} />
             )
-          })}
+          }): "No contact found"}
     </div>
   )
 }
